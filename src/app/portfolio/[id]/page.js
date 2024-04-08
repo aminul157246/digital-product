c1b505b74383b77103d5f9ef5a7a54a8c7f59fc8
aminul157246 <aminul.islam.157246@gmail.com>
2024-04-08 19:37:35 +0600

    add route and layout

---
+++ src/app/portfolio/[id]/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PortfolioDetails = () => {
+    return (
+        <div>
+            Portfolio Details
+        </div>
+    );
+};
+
+export default PortfolioDetails;
